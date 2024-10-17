933672875d1be5ff4935c94f203af67eb9de31a3
tkhnnh <tkhnnh201023@gmail.com>
2024-10-17 14:57:43 +1100

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,11 @@
+l = [8, 3, 5, 1] #The input list
+
+result = 0  #create a variable to store the output
+
+nums = len(l) #get the length of the list
+
+#use for loop to iterate the index of each number in the list then add up each number to 'result'
+for idx in range(nums):
+    result += l[idx] * (10 ** (nums - idx - 1)) #the smaller the index, the higher the number will be
+
+print(result) # Ultimately, I just print the result
